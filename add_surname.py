7ada35b5b53843be5a4c2176894b6367a581a47c
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-08 16:45:34 -0700

    Update add_surname.py

--- add_surname.py
+++ add_surname.py
@@ -4,5 +4,5 @@
 # Description: add surnames to the list of names that start with "K".
 def add_surname(first_name):
     """Define a function named add_surname that takes as a parameter a list of first names."""
-    kardashian_names = [name + "Kardashian" for name in first_name if name.startswith("K")]
+    kardashian_names = [name + " Kardashian" for name in first_name if name.startswith("K")]
     return kardashian_names
